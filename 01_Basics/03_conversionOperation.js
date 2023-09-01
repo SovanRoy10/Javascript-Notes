7a2cecb9b024c0a24b7b7d18c970a4c603961baa
Sovan Roy <110086883+SovanRoy10@users.noreply.github.com>
2023-09-01 04:03:52 +0000

    commit

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -8,8 +8,8 @@ let score = undefined;
 
 let valueInNumber = Number(score);
 
-console.log(typeof score);
-console.log(typeof (valueInNumber), valueInNumber)
+// console.log(typeof score);
+// console.log(typeof (valueInNumber), valueInNumber)
 
 //"33"  => 33
 // "33abc" => NaN
@@ -21,7 +21,34 @@ console.log(typeof (valueInNumber), valueInNumber)
 // let isLoggedIn = "";
 let isLoggedIn = "sh";
 
-console.log(Boolean(isLoggedIn))
+// console.log(Boolean(isLoggedIn))
 
 let someNumber = 23;
-console.log(String(someNumber))
+// console.log(String(someNumber))
+
+
+// ******************************** Operations **********************************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue)
+
+let str1 = "Hello "
+let str2 = "Sovan"
+let str3 = str1 + str2;
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1"+ 2+ 2)
+// console.log(1 + 2+ "2")
+
+// console.log(+ true) // 1
+// console.log(+ "") // 0 
+
+let num1 , num2 , num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+console.log(gameCounter++) // 100
+console.log(++gameCounter) // 102
