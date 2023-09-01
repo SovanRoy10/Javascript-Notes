ac25d071c2c7d9d0cca9e9ecec6beae603297d73
Sovan Roy <110086883+SovanRoy10@users.noreply.github.com>
2023-09-01 03:55:21 +0000

    commit

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score  = "33abc";
+// let score  = null;
+// let score = true;
+let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof (score))
+
+let valueInNumber = Number(score);
+
+console.log(typeof score);
+console.log(typeof (valueInNumber), valueInNumber)
+
+//"33"  => 33
+// "33abc" => NaN
+//true => 1
+// false => 0
+
+// let isLoggedIn = 1;
+// let isLoggedIn = 0;
+// let isLoggedIn = "";
+let isLoggedIn = "sh";
+
+console.log(Boolean(isLoggedIn))
+
+let someNumber = 23;
+console.log(String(someNumber))
