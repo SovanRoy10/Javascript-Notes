5339c5af77aed67dc44789f90dde49724e2c7484
Sovan Roy <110086883+SovanRoy10@users.noreply.github.com>
2023-09-05 19:32:40 +0000

    commit

---
+++ 08_projects/project4/script.js
@@ -0,0 +1,45 @@
+// const random = Math.floor(Math.random()*100)+1;
+
+// const submit = document.querySelector('#subt')
+// const userInput = document.querySelector('#guessField')
+// const guesses = document.querySelector('.guesses')
+// const remaining  = document.querySelector('.lastResult')
+// const lowOrHi  = document.querySelector('.lowOrHI')
+// const start = document.querySelector('.resultParas')
+
+// const p = document.createElement('p')
+
+// let prevGuess = []
+// let numGuess = 1
+
+// let playGame = true
+
+// if(playGame){
+//     submit.addEventListener('click',function(e){
+//         e.preventDefault();
+//     })
+// }
+// function validateGuess(guess){
+
+// }
+
+// function checkGuess(guess){
+
+
+// }
+
+// function displayGuess(guess){
+
+// }
+
+// function displayMessage(message){
+
+// }
+
+// function newGame(){
+
+// }
+
+// function endGame(){
+
+// }
